4e2c92c3db228106eaacb357ac1cd49c10f798c8
estiak <aiubpolin@gmail.com>
2019-02-19 08:39:46 +0600

    first query and mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 const Mutations = {
-    // check if the user is logged in
+    // TODO: check if they are logged in
 
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -9,14 +9,6 @@ const Mutations = {
         console.log(item);
         return item;
     }
-
-    // createDog(parent, args, ctx, info) {
-    //     // create a dog
-    //     global.dog = global.dog || [];
-    //     const newDog = {name: args.name};
-    //     global.dog.push(newDog);
-    //     return newDog;
-    // }
 };
 
 module.exports = Mutations;
