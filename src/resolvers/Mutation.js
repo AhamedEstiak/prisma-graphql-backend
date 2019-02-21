b2ef1ab0d78e80dafe777cfd966d6dc8d70dbfa0
estiak <aiubpolin@gmail.com>
2019-02-21 12:24:42 +0600

    add update schema and resolvers

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -8,6 +8,20 @@ const Mutations = {
 
         console.log(item);
         return item;
+    },
+
+    async updateItem(parent, args, ctx, info) {
+        // first take a copy of updates
+        const updates = { ...args };
+        // remove id from updates
+        delete updates.id;
+        // run the update method
+        return await ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info);
     }
 };
 
