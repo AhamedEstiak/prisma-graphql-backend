37e4d26b0b0775d9a61cbc5f1d863ea7619cbfeb
estiak <aiubpolin@gmail.com>
2019-03-15 08:47:01 +0600

    add signout logix

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     // TODO: check if they are logged in
 
@@ -33,6 +36,57 @@ const Mutations = {
         // TODO
         // 3. delete the item
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+
+    async signup(parent, args, ctx, info) {
+        // lowercase user email
+        args.email = args.email.toLowerCase();
+        // hash user password  
+        const password = await bcrypt.hash(args.password, 10);
+        // Create user in the database
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            },
+        }, info);
+        // Create Jwt token for user
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // We set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+        // Finally return the user to the browser
+        return user;
+    },
+
+    async signin(parent, { email, password }, ctx, info) {
+        // 1. check if there is a user with that email
+        const user = await ctx.db.query.user({ where: { email } });
+        if (!user) {
+            throw new Error(`No user found for email ${email}`);
+        }
+        // 2. check if their password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new Error('Invalid Password!');
+        }
+        // 3. generate the JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // 4. set the cookie with the token
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        // 5. return the user
+        return user;
+    },
+
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token');
+        return { message: "Logout successfull" }
     }
 };
 
